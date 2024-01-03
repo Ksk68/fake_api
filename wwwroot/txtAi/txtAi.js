8f8555584dd442c64826c4f939b0f621479be730
Raul Viela <raulvilela16@gmail.com>
2024-01-03 14:58:14 +0200

    a
    a

--- wwwroot/txtAi/txtAi.js
+++ wwwroot/txtAi/txtAi.js
@@ -1,9 +1,13 @@
-const btn = document.querySelector("#btn")
+document.addEventListener("DOMContentLoaded", function() {
+    const btn = document.querySelector("#btn")
 
-btn.addEventListener("click",function(e){
-    e.preventDefault();
+    btn.addEventListener("click",function(e){
+        e.preventDefault();
 
-    const txt = document.querySelector("#txt")
+        const txt = document.querySelector("#txt")
 
-    console.log(txt)
+        const txt_value = txt.value;
+
+        console.log(txt_value)
+    });
 });
